78c826324aad32ed0e19ff38c1d7c7a93d5c2afe
Hemyani <hdg2512@gmail.com>
2022-11-24 22:44:04 +0530

    Factors

--- Basic Core Programs/Program.cs
+++ Basic Core Programs/Program.cs
@@ -7,7 +7,7 @@ namespace Basic_Core_Problems
     {
         static void Main(string[] args)
         {
-            HarmonicNumber.Number();
+            Factors.Number();
             Console.ReadLine();
         }
     }
