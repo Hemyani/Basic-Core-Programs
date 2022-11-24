0a2b060c205fb14a4f23d62f34b3b711af879ee5
Hemyani <hdg2512@gmail.com>
2022-11-24 22:52:03 +0530

    C# Program to Swap Two Numbers

--- Basic Core Programs/Program.cs
+++ Basic Core Programs/Program.cs
@@ -7,7 +7,7 @@ namespace Basic_Core_Problems
     {
         static void Main(string[] args)
         {
-            QuotientAndRemainder.Remainder();
+            SwapNumbers.Number();
             Console.ReadLine();
         }
     }
