e21ebe44a973376abca2e3d9ebc27b7c1190db43
Hemyani <hdg2512@gmail.com>
2022-11-24 20:39:58 +0530

    Flip Coin and print percentage of Heads and Tails.

--- Basic Core Programs/Program.cs
+++ Basic Core Programs/Program.cs
@@ -9,33 +9,6 @@ namespace Basic_Core_Problems
         {
             FlipCoin.Coin();
             Console.ReadLine();
-
-            LeapYear.Year();
-            Console.ReadLine();
-
-            PowerOf2.Power();
-            Console.ReadLine();
-
-            HarmonicNumber.Number();
-            Console.ReadLine();
-
-            Factor.Number();
-            Console.ReadLine();
-
-            QuotientAndRemainder.Remainder();
-            Console.ReadLine();
-
-            SwapNumbers.Number();
-            Console.ReadLine();
-
-            EvenOrOdd.EvenOdd();
-            Console.ReadLine();
-
-            VowelOrConsonant.charpexercise.Consonant();
-            Console.ReadLine();
-
-            MaximumNum.MaxNum();
-            Console.ReadLine();
         }
     }
 }
